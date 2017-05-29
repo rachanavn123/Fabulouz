eb6214a95dd61a3e2752618f67dae3e3ffc79256
Rachana N <rachanavn123@gmail.com>
2017-05-29 23:07:19 +0530

    Few more changes for carousel

--- src/scripts.js
+++ src/scripts.js
@@ -1,21 +1,26 @@
-$(document).ready(function() {
- 
+$(document).ready(function () {
+
   $(".owl-carousel").owlCarousel({
- 
-      nav : true, // Show next and prev buttons
-      loop: true,
-      items: 1,
-      margin: 10,
-      autoplay: true
-      
- 
-      // "singleItem:true" is a shortcut for:
-      // items : 1, 
-      // itemsDesktop : false,
-      // itemsDesktopSmall : false,
-      // itemsTablet: false,
-      // itemsMobile : false
- 
+
+    nav: true, // Show next and prev buttons
+    loop: true,
+    items: 1,
+    margin: 10,
+    autoplay: true,
+    animateOut: 'slideOutDown',
+    animateIn: 'flipInX',
+    dots: true
+
+
+    // "singleItem:true" is a shortcut for:
+    // items : 1, 
+    // itemsDesktop : false,
+    // itemsDesktopSmall : false,
+    // itemsTablet: false,
+    // itemsMobile : false
+
   });
- 
+
+  $(".owl-prev").text("<");
+  $(".owl-next").text(">");
 });
