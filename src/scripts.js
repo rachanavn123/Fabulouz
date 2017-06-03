3c07fabc60733cbe930e503127b9e953073bd059
Radhika <anuja.kathavate@gmail.com>
2017-06-03 14:26:45 +0530

    Navigation changes for desktop

--- src/scripts.js
+++ src/scripts.js
@@ -1,26 +1,21 @@
-$(document).ready(function () {
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+        nav : true, // Show next and prev buttons
+        loop: true,
+        items: 1,
+        margin: 10,
+        autoplay: true
+        // "singleItem:true" is a shortcut for:
+        // items : 1,
+        // itemsDesktop : false,
+        // itemsDesktopSmall : false,
+        // itemsTablet: false,
+        // itemsMobile : false
+    });
 
-  $(".owl-carousel").owlCarousel({
-
-    nav: true, // Show next and prev buttons
-    loop: true,
-    items: 1,
-    margin: 10,
-    autoplay: true,
-    animateOut: 'slideOutDown',
-    animateIn: 'flipInX',
-    dots: true
-
-
-    // "singleItem:true" is a shortcut for:
-    // items : 1, 
-    // itemsDesktop : false,
-    // itemsDesktopSmall : false,
-    // itemsTablet: false,
-    // itemsMobile : false
-
-  });
-
-  $(".owl-prev").text("<");
-  $(".owl-next").text(">");
+    // $("#collection-menu, #scocial-menu").on("click", function(e) {
+    //     console.log("click");
+    //     e.preventDefault();
+    //     e.stopPropagation();
+    // });
 });
