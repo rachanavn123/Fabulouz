330f2d440d63cab8a09d0b5123e5f479ac54cb45
Radhika <anuja.kathavate@gmail.com>
2017-06-03 17:32:14 +0530

    Navigation for tablet and mobile

--- src/scripts.js
+++ src/scripts.js
@@ -18,4 +18,25 @@ $(document).ready(function() {
     //     e.preventDefault();
     //     e.stopPropagation();
     // });
+
+    function mediaSize() {
+		if (window.matchMedia(
+            '(min-width: 768px) and (max-width: 979px) and (orientation: portrait)')
+            .matches) {
+	    	$("#collectionLevel1, #collectionLevel2").addClass("in");
+		} else {
+            $("#collectionLevel1, #collectionLevel2").removeClass("in");
+        }
+	};
+
+    /* Call the function */
+    mediaSize();
+
+    /* Attach the function to the resize event listener */
+    window.addEventListener('resize', mediaSize, false);
+
+    $('#nav-icon3').click(function(){
+		$(this).toggleClass('open');
+        $(".nav.nav-stacked").toggleClass("open");
+	});
 });
